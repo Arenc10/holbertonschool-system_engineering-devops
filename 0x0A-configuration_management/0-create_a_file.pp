4544c56e61c44dac56e195e6b66c7ed4a04b2f9f
Arenc10 <arenc.palluqi@yahoo.com>
2022-08-12 02:52:40 -0700

    First puppet manifestation creating a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+# A puppet manifest that creates a file in the /tmp directory
+file { '/tmp/school':
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data'
+}
