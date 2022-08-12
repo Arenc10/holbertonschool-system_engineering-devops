a27f152efd4d5e6114a92957d435255a6596bc84
Arenc10 <arenc.palluqi@yahoo.com>
2022-08-12 10:56:58 -0700

    Added path

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
 # A puppet manifest that creates a file in the /tmp directory
-file { '/tmp/school':
+file { 'school':
   ensure  => 'present'
+  path    => '/tmp/school'
   content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
