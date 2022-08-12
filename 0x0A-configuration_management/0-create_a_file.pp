fe1fabbd720bc054df71852f2ff8545b44d5bef2
Arenc10 <arenc.palluqi@yahoo.com>
2022-08-12 05:37:00 -0700

    Fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,6 @@
 # A puppet manifest that creates a file in the /tmp directory
 file { '/tmp/school':
+  ensure  => 'present'
   content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
